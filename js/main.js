9eda064aa06146ae91a7cd2cbd45ef4ba393ffc5
Chkeka <Chkeka@users.noreply.github.com>
2016-07-15 17:32:45 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         subMenu.height(subMenu.childElementCount * 54.8);
         var subHeight = subMenu.height();
         if(menu.is('#store')) menu.height(548)
-        else if (menu.is('#products')) menu.height(274);
+        else if (menu.is('#products')) menu.height(272);
         if(subHeight > menu.height()) {
             menu.height(subHeight);
         }
@@ -20,5 +20,14 @@ $(document).ready(function() {
         subMenu.offset(subOffset);
     });
     
-    
+    $('#subscribe .form-group input').on('click', function() {
+        var inputField = $(this);
+        inputField.closest('.form-group').find('label').css('opacity', '.5');
+        $(document).click(function(event){
+           if (!$(event.target).is(inputField)) $(inputField).closest('.form-group').find('label').css('opacity', '1'); 
+        });
+        inputField.on('input', function() {
+            inputField.closest('.form-group').find('label').css('visibility', 'hidden');
+        });
+    });
 });
